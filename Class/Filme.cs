d1e01c14bbc6767b5d6598a8f77e787c16b0b90a
hunteradv <gustavopaulody@gmail.com>
2021-10-27 23:37:25 -0300

    first commit

---
+++ Class/Filme.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaFilmesSeries
+{
+    public class Filme : EntidadeBase
+    {
+        //getters and setters
+        private string nomeFilme { get; set; }
+        private Genero generoFilme { get; set; }
+        private int anoLancamentoFilme { get; set; }
+        private float notaFilme { get; set; }
+        private string avaliacaoFilme { get; set; }
+
+        //metods
+        public Filme(int id, Genero generoFilme, int anoLancamentoFilme, float notaFilme, string avaliacaoFilme)
+        {
+            this.id = id;
+            this.generoFilme = generoFilme;
+            this.anoLancamentoFilme = anoLancamentoFilme;
+            this.notaFilme = notaFilme;
+            this.avaliacaoFilme = avaliacaoFilme;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + this.generoFilme + Environment.NewLine;
+            retorno += "Título: " + this.nomeFilme + Environment.NewLine;
+            retorno += "Ano de lançamento: " + this.anoLancamentoFilme + Environment.NewLine;
+            retorno += "Nota: " + this.notaFilme + Environment.NewLine;
+            retorno += "Avalicação: " + this.avaliacaoFilme + Environment.NewLine;
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.nomeFilme;
+        }
+
+        public int retornaId()
+        {
+            return this.id;
+        }
+    }
+}
---
+++ Enum/Genero.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaFilmesSeries
+{
+    public enum Genero
+    {
+        Acao = 1,
+        Comedia = 2,
+        Drama = 3,
+        Terror = 4,
+        Aventura = 5,
+        Documentario = 6,
+        Faroeste = 7,
+        Espionagem = 8,
+        FiccaoCientifica = 9,
+        Romance = 10,
+        Musical = 11,
+        Suspense = 12,
+        Fantasia = 13,
+    }
+}
